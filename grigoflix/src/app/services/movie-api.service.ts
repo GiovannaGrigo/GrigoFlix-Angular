bb9c4fd90aea9703a5402f7dd77e286008c5f942
giovannagrigo <giovanna1grigo@gmail.com>
2025-05-23 20:34:16 -0300

    Consumo do serviço de filmes pelo frontend para exibição dos filmes em um carrossel

--- grigoflix/src/app/services/movie-api.service.ts
+++ grigoflix/src/app/services/movie-api.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,6 +9,17 @@ export class MovieApiService {
 
   constructor(private http: HttpClient) { }
 
+  baseUrl = "https://api.themoviedb.org/3";
+  options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0M2U1MGUyM2RlODU0MTEzNzJmY2IwYWI3Yzg2MjIxOCIsIm5iZiI6MTczMjA0MjA0OS4xNjk5OTk4LCJzdWIiOiI2NzNjZGQ0MTYwYjdiM2JjOTRhMGQzZTgiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.ZH146obDT_1TQf6DlKEXhkFjmt96unbu2M6B6J8h4iQ'
+    }
+  };
   
-
+  // Banner de Midias da Semana
+  bannerApiData(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/trending/all/week?language=pt-br`, this.options);
+  }
 }
